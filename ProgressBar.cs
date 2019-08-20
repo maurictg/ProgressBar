c262ad75a40c02f198b556512bb4fa9eca889323
Henkje <henkje@pm.me>
2019-08-20 10:20:46 +0200

    Update ProgressBar.cs

--- ProgressBar.cs
+++ ProgressBar.cs
@@ -1,21 +1,15 @@
 using System;
+using System.Diagnostics;
 
-public class ProgressBar
+namespace ProgressBar
+{
+    class ProgressBar
     {
-        public int Blocks = 20,
-            Max = 100,
-            Min = 0,
-            PositionX,
-            PositionY;
+        public int BlocksCount = 20,
+            Max, Min,
+            PositionX, PositionY;
 
-        long prevTicks;
-        long averageTicks;
-        long updates = 0;
-        long totalTicks = 0;
-
-        string Message;
-        char Symbol = '=';
-
+        public char Symbol = '=';
         private int progress;
         public int Progress
         {
@@ -23,69 +17,42 @@ public class ProgressBar
             set
             {
                 progress = value;
-                PreUpdate();
+                Update();
             }
         }
 
-        System.Timers.Timer timer = new System.Timers.Timer(500);
+        private int lastProgress;
+        private decimal average;
+        private Stopwatch stopwatch;
 
-
-        public ProgressBar(int max = 100, int min = 0, string message = "Loading...", int blocks = 20)
+        public ProgressBar(int max = 100, int min = 0, int blocksCount = 20, int positionX = 0, int positionY = 0)
         {
             Max = max;
             Min = min;
-            Message = message;
-            Blocks = blocks;
-            prevTicks = 0;
-            timer.Start();
-            timer.Elapsed += delegate { Update(); };
-        }
+            BlocksCount = blocksCount;
+            PositionX = positionX;
+            PositionY = positionY;
 
-        private void PreUpdate()
-        {
-            updates++;
-            long ticks = DateTime.UtcNow.Ticks;
-            //Algoritme 
-
-            prevTicks = (prevTicks < 1) ? ticks : prevTicks;
-
-            long elapsedTicks = ticks - prevTicks;
-            totalTicks += elapsedTicks;
-            averageTicks = (totalTicks + elapsedTicks) / updates;
+            lastProgress = min;
+            stopwatch = Stopwatch.StartNew();
         }
 
         private void Update()
         {
-            decimal c = (decimal)progress;
-            decimal t = (decimal)Max;
-            decimal percentage = (c / t) * 100;
-            int blocks = Convert.ToInt32((c/t)*Blocks);
+            var percentage = (decimal)progress / Max * 100;
+            int blocks = (int)((decimal)progress / Max * BlocksCount);
 
-            long ticks = DateTimeOffset.UtcNow.Ticks;
-
-            long ticksNeeded = averageTicks * Max;
-            long ticksLeft = ticksNeeded - totalTicks;
-            TimeSpan timeLeft = TimeSpan.FromTicks(ticksLeft);
-            //hoe convert je deze naar x minuten en x seconde, of wanneer het minder dan een minuut is naar x seconde
-
-            int hours = (timeLeft.TotalMinutes > 60) ? timeLeft.Hours : 0; 
-            int minutes = (timeLeft.TotalSeconds > 60) ? timeLeft.Minutes : 0;
-            int seconds = timeLeft.Seconds;
-
-            string timestring = $"{hours.ToString().PadLeft(hours.ToString().Length,'0')}:{minutes.ToString().PadLeft(minutes.ToString().Length, '0')}:{seconds.ToString().PadLeft(seconds.ToString().Length, '0')}";
-
-            Console.SetCursorPosition(PositionX, PositionY);
-
-            string progressBar = $"{Message} [{new string(Symbol, blocks).PadRight(Blocks,' ')}] ({percentage.ToString("0.0")}%) [{timestring} left]";
-            Console.WriteLine(progressBar);
-
-            prevTicks = ticks;
-
-            if (progress >= Max)
+            if(stopwatch.ElapsedMilliseconds >= 1000 || average == 0)
             {
-                timer.Stop();
-                Console.WriteLine("Done!");
+                average = (average + (decimal)(progress - lastProgress) / stopwatch.ElapsedMilliseconds) / 2;
+                lastProgress = progress;
+                stopwatch.Restart();
             }
-                
+            TimeSpan timeLeft = TimeSpan.FromMilliseconds((int)((Max - progress) / average));
+
+            string progressBar = $"[{new string(Symbol, blocks).PadRight(BlocksCount, ' ')}] ({percentage.ToString("0.0")}%) [{string.Format("{0:h\\:m\\:ss}",timeLeft)}]";
+            Console.SetCursorPosition(PositionX,PositionY);
+            Console.WriteLine(progressBar);
         }
     }
+}
